b5096ada72725e548479e828d94d9b50c924575c
Gz <gz.us.to@gmail.com>
2022-04-18 12:12:47 +0900

    [tf] setup aws_vpc

---
+++ infra/terraform/vpc.tf
@@ -0,0 +1,19 @@
+variable "region" {
+  default     = "ap-northeast-3"
+  description = "AWS region"
+}
+
+data "aws_availability_zones" "available" {
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "terraform-101"
+  }
+}
