8d4cc38e146e7581b39da88ff3736ae0eb813f5a
Gz <gz.us.to@gmail.com>
2022-04-18 15:25:06 +0900

    [infra] Add security group for vpc

--- infra/terraform/vpc.tf
+++ infra/terraform/vpc.tf
@@ -76,3 +76,60 @@ resource "aws_route_table" "main" {
     "Name" = "main"
   }
 }
+
+resource "aws_security_group" "sg_main_external" {
+  name = "sg_main_external"
+  description = "sg_main_external"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    cidr_blocks = [ "0.0.0.0/0" ]
+    protocol = "tcp"
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    cidr_blocks = [aws_subnet.main_subnet_bastion.cidr_block]
+    protocol = "tcp"
+  }
+}
+
+resource "aws_security_group" "sg_main_internal" {
+  name = "sg_main_internal"
+  description = "sg_main_internal"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 3306
+    to_port = 3306
+    cidr_blocks = aws_subnet.main_subnet_public.*.cidr_block
+    protocol = "tcp"
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    cidr_blocks = [aws_subnet.main_subnet_bastion.cidr_block]
+    protocol = "tcp"
+  }
+}
+
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com"
+}
+
+resource "aws_security_group" "sg_main_bastion" {
+  name = "sg_main_bastion"
+  description = "sg_main_bastion"
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+  }
+}
